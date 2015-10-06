e08a70ec3b925ee78039887600365e2559e97a85
Nickolas Nikolic <nickolasnikolic@gmail.com>
2015-10-06 17:17:54 -0500

    amazon fix

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -5,12 +5,12 @@ artApp.controller('HomeController', ['$scope', '$state', 'globals', function($sc
 artApp.controller('PhotoController', ['$scope', '$state', 'globals', function($scope, $state, globals) {
   document.title = 'pleasant.space - photos'; //set the page title
   var productList = [
-      'B00YRUB4TA',
-      'B00RV5X2MA',
-      'B0132X2JQ4',
-      'B00A0R6HS2',
-      'B011M7FQ0S',
-      'B00NCV6JG8'
+      'B00EO5QY84',
+      'B00EILJKIA',
+      'B00ECJYYWA',
+      'B00EW4APKU',
+      'B00XAQX8O6',
+      'B00E83VN9C'
   ];
 
   $scope.art = globals.getItemsFromAmazon( productList );
